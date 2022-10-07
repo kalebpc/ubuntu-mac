f4dc339253da7050e2d9e0513dc0d4825cb1e52f
kalebpc <113309697+kalebpc@users.noreply.github.com>
2022-10-07 16:34:32 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt-get update
+yes | apt upgrade && apt-get install git
+#Install and setup universal firewall
+sh Win10-MacOs-Ubuntu-laptop/ufw.sh
+#Install gnome-tweaks, extensions
+yes | apt-get install gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager
+
+####////Have to manually open Extension Manager > search for the extensions and install !!!!
+
+#WhiteSur theme install
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+./WhiteSur-gtk-theme/install.sh -c Dark -t all -N glassy -s 220
+
+####////still need to add background change
+
+#WhiteSur icon theme
+git clone https://github.com/zayronxio/Mkos-Big-Sur.git
+mkdir .icons
+cp -r Mkos-Big-Sur .icons
+#Configure gnome tweaks
+chmod +x Win10-MacOs-Ubuntu-laptop/gsettings.sh
+sleep 1
+./WhiteSur-gtk-theme/tweaks.sh -f monterey -s
+echo 'Run    ./Win10-MacOs-Ubuntu-laptop/gsettings.sh   to configure gnome, extensions!!!'
