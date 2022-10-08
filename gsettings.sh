b0fd3fb070fe58219ffd2ebdcf2b4235df68afe0
kalebpc <113309697+kalebpc@users.noreply.github.com>
2022-10-08 07:40:51 -0400

    Update gsettings.sh

--- gsettings.sh
+++ gsettings.sh
@@ -4,5 +4,7 @@ gnome-extensions disable ding@rastersoft.com
 gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Mkos-Big-Sur'
 gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-Dark'
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height 'false'
 
-echo 'OPEN Extension Manager to search for specified extensions and install them!!'
+echo 'OPEN Extension Manager to search and install "User Themes, Blur-my-shell, Compiz alike magic lamp effect" extensions'
